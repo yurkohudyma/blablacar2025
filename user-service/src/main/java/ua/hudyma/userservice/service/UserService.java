a9e7a640fb9c71f23640ad1541b4d0c45cc946b3
yurkohudyma <hudyma@gmail.com>
2025-06-11 21:47:03 +0300

    init commit

---
+++ user-service/src/main/java/ua/hudyma/userservice/service/UserService.java
@@ -0,0 +1,11 @@
+package ua.hudyma.userservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class UserService {
+}
