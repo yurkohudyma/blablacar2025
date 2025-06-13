d9d05d38be6544531e3ba2045fd784cf49dc3fff
yurkohudyma <hudyma@gmail.com>
2025-06-13 19:16:21 +0300

    four m/services fetch config from server and are traced by Eureka Discovery

--- discovery-service/src/main/java/ua/hudyma/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/ua/hudyma/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package ua.hudyma.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
--- trip-service/src/main/java/ua/hudyma/tripservice/TripServiceApplication.java
+++ trip-service/src/main/java/ua/hudyma/tripservice/TripServiceApplication.java
@@ -2,7 +2,9 @@ package ua.hudyma.tripservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TripServiceApplication {
     public static void main(String[] args) {
