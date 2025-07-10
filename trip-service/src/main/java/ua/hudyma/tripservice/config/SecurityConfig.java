3e764f6051b1e7f9b7778bc7fe2ed632e89aba42
yurkohudyma <hudyma@gmail.com>
2025-07-10 19:56:54 +0300

    attached keycloak security for trip-service fixed /public endpoint securing and 401 fixed no token transmitting via FeignClientInterceptor

--- trip-service/src/main/java/ua/hudyma/tripservice/config/SecurityConfig.java
+++ trip-service/src/main/java/ua/hudyma/tripservice/config/SecurityConfig.java
@@ -1,13 +1,12 @@
-package ua.hudyma.userservice.config;
+package ua.hudyma.tripservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
@@ -17,12 +16,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/drivers/public/**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
-                        )
-                );
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
         return http.build();
     }
